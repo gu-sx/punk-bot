b7db46bef1692684a1b062890127a5efffb64bc6
gu-sx <gusx@pm.me>
2022-10-23 23:54:42 +0200

    feat: update member role to applicant when joining the server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 
-const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { ActivityType, Client, Collection, Events, GatewayIntentBits, GuildMember } = require('discord.js');
 const AbsenceCommand = require('./commands/absence');
 
 require('dotenv').config();
@@ -11,7 +11,14 @@ if (!BotToken) {
 }
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ 
+   intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMembers,
+	],
+});
 
 client.commands = new Collection();
 
@@ -19,8 +26,16 @@ client.commands.set(AbsenceCommand.data.name, AbsenceCommand.execute);
 
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
-client.once(Events.ClientReady, (c: any) => {
+client.once(Events.ClientReady, async (c: any) => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
+	await c.user.setActivity('a whales mating call', { type: ActivityType.Listening });
+});
+
+client.on(Events.GuildMemberAdd, async (guildMember: any) => {
+  const applicantRole = await guildMember.guild.roles.fetch('1033851034994475068');
+  await guildMember.edit({ roles: [applicantRole] });
+  // const channel = await guildMember.createDM(true);
+  // await channel.send('Hello and welcome to Punk!');
 });
 
 client.on(Events.InteractionCreate, async (interaction: any) => {
